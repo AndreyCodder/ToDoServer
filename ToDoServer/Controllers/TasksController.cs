3ba65271f3f50c14dcfae4f74f4fe24b693ba20a
AndreyCodder <vice.ya5@gmail.com>
2022-09-29 22:54:11 +0300

    Connected to db. Need creation tables from code

--- ToDoServer/Controllers/TasksController.cs
+++ ToDoServer/Controllers/TasksController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.Intrinsics.X86;
 using ToDoServer.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -26,6 +28,37 @@ namespace ToDoServer.Controllers
                     return new DemoTaskList().tasksDemo;
                     break;
             }
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public IEnumerable<ToDoServer.Models.Task> GetList()
+        {
+            //return new DemoTaskList().tasksDemo;
+            using TaskContext db = new TaskContext();
+            // создаем два объекта User
+            ToDoServer.Models.Task t1 = new ToDoServer.Models.Task { Id = Guid.NewGuid(), Text = "1" };
+            ToDoServer.Models.Task t2 = new ToDoServer.Models.Task { Id = Guid.NewGuid(), Text = "2" };
+            ToDoServer.Models.Task t3 = new ToDoServer.Models.Task { Id = Guid.NewGuid(), Text = "and" };
+            ToDoServer.Models.Task t4 = new ToDoServer.Models.Task { Id = Guid.NewGuid(), Text = "3" };
+
+
+            // добавляем их в бд
+            db.Tasks.Add(t1);
+            db.Tasks.Add(t2);
+            db.Tasks.Add(t3);
+            db.Tasks.Add(t4);
+            db.SaveChanges();
+
+
+            // получаем объекты из бд и выводим на консоль
+            var tasks = db.Tasks.ToList();
+            //Console.WriteLine("Список объектов:");
+            return tasks;
+            //foreach (ToDoServer.Models.Task u in tasks)
+            //{
+            //    Console.WriteLine("{0}.{1} - {2}", u.Id, u.Id, u.Text);
+            //}
         }
 
         [HttpGet]
---
+++ ToDoServer/Models/DbContext/TaskContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ToDoServer.Models
+{
+    class TaskContext : DbContext
+    {
+        public TaskContext()
+        {
+        }
+
+        public TaskContext(DbContextOptions<TaskContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("MyConnection"));
+        }
+    }
+}
